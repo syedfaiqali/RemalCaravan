7bc2c1da9e922cdaa480abed5e04d30beaea0e90
Syed Faiq Ali Jafri <faiqalisyed00@gmail.com>
2026-04-11 00:06:05 +0500

    main: partner page

--- src/pages/PartnerPage.tsx
+++ src/pages/PartnerPage.tsx
@@ -1,15 +1,9 @@
 import PartnerWithUsSection from '../components/sections/gallery/PartnerWithUsSection'
-import AdventureCTASection from '../components/common/AdventureCTASection'
 
 function PartnerPage() {
   return (
     <>
       <PartnerWithUsSection />
-      <AdventureCTASection
-        title="Build the Future of RV Travel"
-        subtitle="Join our partner network and grow with Remal Caravans."
-        // buttonText="Start the Partnership"
-      />
     </>
   )
 }
